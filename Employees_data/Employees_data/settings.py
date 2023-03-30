ba43894a2acbce469fa2bf683fa84911115fe024
Mohd Sanabil <mohdsanabil4557@gmail.com>
2023-03-30 10:37:53 +0530

    added csrf_trusted_origins

--- Employees_data/Employees_data/settings.py
+++ Employees_data/Employees_data/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://employees-data-project.up.railway.app/'
+]
